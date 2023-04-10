349273133cc50a97ec7adb6dc7cb123e22fd2d48
Habtesellasie <havecodesoft@gmail.com>
2023-04-10 17:28:41 +0300

    it

--- shop.js
+++ shop.js
@@ -2,6 +2,12 @@ const cartCount = document.querySelector(".cart-count");
 const btnClear = document.querySelector(".clear-it");
 const countNum = document.querySelector(".count");
 const addToCart = document.querySelectorAll(".cart-add-btn");
+const loader = document.querySelector(".load-it");
+
+window.addEventListener("load", function () {
+  loader.style.overflow = "hidden";
+  loader.style.display = "none";
+});
 
 let counter = parseInt(localStorage.getItem("counter")) || 0;
 
